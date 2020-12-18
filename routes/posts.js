d97dfa54dd7a4a4af39f6bbbdc8c46df921cd90b
Salman Zafar <salman.zafar@innowi.com>
2020-12-18 13:57:23 +0500

    fetch all posts and Post Model Schema

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const Post = require('../Models/Post');
 
 router.get('/', async (req, res) => {
-    res.json('All posts')
+    try{
+        const posts = await Post.find();
+        res.json(posts)
+    }catch (e) {
+        res.status(500)
+        res.json(e)
+    }
 })
 
 module.exports = router;
